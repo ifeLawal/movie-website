f0463fa98baa3ea91eb6ab74f905652a3c6e281c
Ifeoluwa Lawal <il693@nyu.edu>
2020-09-07 16:58:10 -0400

    Updates

---
+++ precache-manifest.0b95a79bdb11ca2e39054395a38ae6fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f4c37e0dec5c811d1e3470733c4c767",
+    "url": "/ifelawal.github.io/movie-website/index.html"
+  },
+  {
+    "revision": "91df61bec6757c58fea5",
+    "url": "/ifelawal.github.io/movie-website/static/css/main.aa2c31b0.chunk.css"
+  },
+  {
+    "revision": "fcc2d37c479291b07570",
+    "url": "/ifelawal.github.io/movie-website/static/js/2.4d508fba.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/ifelawal.github.io/movie-website/static/js/2.4d508fba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91df61bec6757c58fea5",
+    "url": "/ifelawal.github.io/movie-website/static/js/main.f63af1e8.chunk.js"
+  },
+  {
+    "revision": "7a0e1db4bb833860ec13",
+    "url": "/ifelawal.github.io/movie-website/static/js/runtime-main.ad27f9f8.js"
+  }
+]);
